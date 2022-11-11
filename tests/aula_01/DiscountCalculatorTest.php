d86ebd4521503791ec1f2a4dadc14de104521094
Paulo Daniel Dorensbach <dorensbach@gmail.com>
2022-11-10 23:46:15 -0300

    Criar uma ferramenta própia de testes
    Signed-off-by: Paulo Daniel Dorensbach <dorensbach@gmail.com>

---
+++ tests/aula_01/DiscountCalculatorTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+class DiscountCalculatorTest
+{
+
+    public function ShouldApply_WhenValueIsAboveTheMinimumTest() {
+
+        $discountCalculator = new DiscountCalculator();
+
+        $totalValue = 130;
+        $totalWithDiscount = $discountCalculator->apply($totalValue);
+
+        $expectedValue = 110;
+        $this->assertEquals($expectedValue, $totalWithDiscount);
+
+    }
+
+    public function ShouldNotApply_WhenValueIsBellowTheMinimumTest() {
+
+        $discountCalculator = new DiscountCalculator();
+
+        $totalValue = 90;
+        $totalWithDiscount = $discountCalculator->apply($totalValue);
+
+        $expectedValue = 90;
+        $this->assertEquals($expectedValue, $totalWithDiscount);
+
+    }
+
+    public function assertEquals($expectedValue, $actualValue) {
+
+        if ($expectedValue !== $actualValue) {
+            $message = 'Expected: ' . $expectedValue . 'but got: ' . $actualValue;
+            throw new \Exception($message);
+        }
+
+        echo "Test passed! \n";
+
+    }
+}
